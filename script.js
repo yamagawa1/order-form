755e8c7cb9927fca2c643e85ffb04a9446ac0ce4
yamagawa1 <isha23143@ict.shimanet.ed.jp>
2025-07-15 12:14:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/あなたのGASのURL/exec'; // GASのURLに差し替え
+const scriptURL = 'https://script.google.com/macros/s/https://script.google.com/a/macros/ict.shimanet.ed.jp/s/AKfycbzusBRQPM3huqi0vPy6GOb2RSmFod1m21L3QFkcPk51NAkkx_UeF1BKrvdc5nFbKdIq/exec/exec'; // GASのURLに差し替え
 const liffId = '2007744995-kMOg452M'; // LIFF IDに差し替え
 
 document.addEventListener("DOMContentLoaded", async () => {
